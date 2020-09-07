b5a77385814cfdac1c24683962afa5c27ad154cf
Your Name <you@example.com>
2020-09-07 17:36:42 +0600

    Add board clearningmethod

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import unittest
+import copy
 import itertools
+import unittest
 
 
+b = 'b'
 d = 'd'
 s = '-'
 LEFT = 'LEFT'
@@ -9,6 +11,13 @@ RIGHT = 'RIGHT'
 UP = 'UP'
 DOWN = 'DOWN'
 CLEAN = 'CLEAN'
+
+
+def get_nearest_vertex(posr, posc, vertexes):
+    return min(
+        [(v, calculate_distance_for_path([[posr, posc], v])) for v in vertexes],
+        key=lambda x: x[1]
+    )[0]
 
 
 def get_full_path(bot_coord, vertex_path):
@@ -48,32 +57,48 @@ def vertex_in_square(vertex, square):
     return False
 
 
-def get_nearest_vertex(posr, posc, vertexes):
-    return min(
-        [(v, calculate_distance_for_path([[posr, posc], v])) for v in vertexes],
-        key=lambda x: x[1]
-    )[0]
+def get_nearest_vertexes(posr, posc, board=None, base_vertex_list=None, frame_vertex=None):
+    if board is not None and base_vertex_list is None:
+        if frame_vertex is not None:
+            vertex_m, vertex_n = frame_vertex
 
+            start_m = min(posr, vertex_m)
+            start_n = min(posc, vertex_n)
 
-def get_depending_vertexes_for_original_vertex(vertex, posr, posc, board):
-    vertex_m, vertex_n = vertex
+            end_m = max(posr, vertex_m)
+            end_n = max(posc, vertex_n)
+        else:
+            start_m = 0
+            start_n = 0
 
-    start_m = min(posr, vertex_m)
-    start_n = min(posc, vertex_n)
+            end_m = len(board) - 1
+            end_n = len(board[0]) - 1
 
-    end_m = max(posr, vertex_m)
-    end_n = max(posc, vertex_n)
+        base_vertex_list = []
+        for i in range(start_m, end_m + 1):
+            for j in range(start_n, end_n + 1):
+                if board[i][j] == d:
+                    base_vertex_list.append(tuple([i, j]))
 
-    depending_vertexes = []
-    for i in range(start_m, end_m + 1):
-        for j in range(start_n, end_n + 1):
-            if (i, j) == (vertex_m, vertex_n):
-                continue
+    vertex_list = []
+    for i, j in base_vertex_list:
+        remove_vertexes = []
+        accept_vertex = True
 
-            if board[i][j] == d:
-                depending_vertexes.append(tuple([i, j]))
+        for v in vertex_list:
+            if vertex_in_square((i, j), (v, (posr, posc))):
+                remove_vertexes.append(v)
 
-    return depending_vertexes
+            if vertex_in_square(v, ((i, j), (posr, posc))):
+                accept_vertex = False
+
+        if accept_vertex:
+            vertex_list.append(tuple([i, j]))
+
+        for vertex_to_remove in remove_vertexes:
+            vertex_list.remove(vertex_to_remove)
+
+    return vertex_list
 
 
 def get_independent_vertexes(posr, posc, board):
@@ -109,25 +134,33 @@ def get_independent_vertexes(posr, posc, board):
     return independent_vertexes_list
 
 
-def get_nearest_vertexes(posr, posc, original_vertexes):
+def get_depending_vertexes_for_original_vertex(vertex, posr, posc, board):
+    vertex_m, vertex_n = vertex
+
+    start_m = min(posr, vertex_m)
+    start_n = min(posc, vertex_n)
+
+    end_m = max(posr, vertex_m)
+    end_n = max(posc, vertex_n)
+
+    depending_vertexes = []
+    for i in range(start_m, end_m + 1):
+        for j in range(start_n, end_n + 1):
+            if (i, j) == (vertex_m, vertex_n):
+                continue
+
+            if board[i][j] == d:
+                depending_vertexes.append(tuple([i, j]))
+
+    return depending_vertexes
+
+
+def get_vertex_list(board):
     vertex_list = []
-    for i, j in original_vertexes:
-        remove_vertexes = []
-        accept_vertex = True
-
-        for v in vertex_list:
-            if vertex_in_square((i, j), (v, (posr, posc))):
-                remove_vertexes.append(v)
-
-            if vertex_in_square(v, ((i, j), (posr, posc))):
-                accept_vertex = False
-
-        if accept_vertex:
-            vertex_list.append(tuple([i, j]))
-
-        for vertex_to_remove in remove_vertexes:
-            vertex_list.remove(vertex_to_remove)
-
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == d:
+                vertex_list.append(tuple([i, j]))
     return vertex_list
 
 
@@ -137,18 +170,59 @@ def next_move(posr, posc, dim_x, dim_y, board):
         return CLEAN
 
     independent_vertexes = get_independent_vertexes(posr, posc, board)
-    nearest_independent_vertex = get_nearest_vertex(posr, posc, independent_vertexes)
-    #print('independent_vertexes', independent_vertexes)
-    #print('nearest_independent_vertex', nearest_independent_vertex)
+    nearest_vertex_list = get_nearest_vertexes(posr, posc, board=board)
+    vertex_list = list(set(nearest_vertex_list + independent_vertexes))
 
-    depending_vertexes = get_depending_vertexes_for_original_vertex(
-        nearest_independent_vertex, posr, posc, board)
-    #print('depending_vertexes', depending_vertexes)
+    if not vertex_list:
+        return
 
-    base_vertexes = depending_vertexes + [nearest_independent_vertex]
-    vertex_list = get_nearest_vertexes(posr, posc, base_vertexes)
-    #print('base_vertexes', base_vertexes)
-    #print('vertex_list', vertex_list)
+    vertex_permutations = itertools.permutations(vertex_list)
+    min_path = None
+    min_distance = None
+
+    for vertex_path in vertex_permutations:
+        full_vertex_path = get_full_path((posr, posc), vertex_path)
+        distance = calculate_distance_for_path(full_vertex_path)
+        #print(full_vertex_path)
+        #print(distance)
+
+        if min_path is None:
+            min_path = vertex_path
+            min_distance = distance
+        else:
+            if distance < min_distance:
+                min_path = vertex_path
+                min_distance = distance
+
+    if min_path is not None:
+        min_i, min_j = min_path[0]
+        if min_i - posr != 0:
+            if min_i - posr < 0:
+                print(UP)
+                return UP
+            else:
+                print(DOWN)
+                return DOWN
+
+        if min_j - posc != 0:
+            if min_j - posc > 0:
+                print(RIGHT)
+                return RIGHT
+            else:
+                print(LEFT)
+                return LEFT
+
+    return
+
+
+def next_move_reference(posr, posc, dim_x, dim_y, board):
+    if board[posr][posc] == d:
+        print(CLEAN)
+        return CLEAN
+
+    vertex_list = get_vertex_list(board)
+    if not vertex_list:
+        return
 
     vertex_permutations = itertools.permutations(vertex_list)
     min_path = None
@@ -252,7 +326,7 @@ class TestFunctions(unittest.TestCase):
         posr, posc = 2, 2
         expected = ((0, 2), (1, 3))
 
-        result = get_nearest_vertexes(posr, posc, vertexes)
+        result = get_nearest_vertexes(posr, posc, base_vertex_list=vertexes)
 
         self.assertEqual(set(expected), set(result))
 
@@ -261,11 +335,11 @@ class TestFunctions(unittest.TestCase):
         posr, posc = 2, 2
         expected = ((4, 0), (0, 2), (1, 3))
 
-        result = get_nearest_vertexes(posr, posc, vertexes)
+        result = get_nearest_vertexes(posr, posc, base_vertex_list=vertexes)
 
         self.assertEqual(set(expected), set(result))
 
-    def test_next_move(self):
+    def test_next_move1(self):
         board = (
             (s, s, s, s, s),
             (s, s, s, s, s),
@@ -279,6 +353,67 @@ class TestFunctions(unittest.TestCase):
         result = next_move(posr, posc, None, None, board)
 
         self.assertEqual(set(expected), set(result))
+
+    @staticmethod
+    def get_bot_coord(board, posr, posc, move):
+        if move == UP:
+            return posr - 1, posc
+        elif move == DOWN:
+            return posr + 1, posc
+        elif move == LEFT:
+            return posr, posc - 1
+        elif move == RIGHT:
+            return posr, posc + 1
+        else:
+            return posr, posc
+
+    def clean_board(self, base_board, posr, posc, next_move_function):
+        board = copy.deepcopy(base_board)
+        move_count, max_move_count = 0, 300
+
+        for move_count in range(max_move_count):
+            move = next_move_function(posr, posc, None, None, board)
+            if move is None:
+                break
+
+            posr, posc = self.get_bot_coord(board, posr, posc, move)
+            if board[posr][posc] == d and move == CLEAN:
+                board[posr][posc] = s
+            move_count += 1
+
+        return move_count
+
+    def test_next_move2(self):
+        board = [
+            [d, s, s, s, d],
+            [s, s, s, s, s],
+            [s, s, d, s, s],
+            [s, s, s, s, s],
+            [s, s, s, s, d],
+        ]
+        posr, posc = 2, 0
+
+        expected_move_count = self.clean_board(board, posr, posc, next_move_reference)
+        result_move_count = self.clean_board(board, posr, posc, next_move)
+
+        self.assertEqual(expected_move_count, result_move_count)
+
+    def test_next_move3(self):
+        board = [
+            [s, s, s, s, s, s, s, s, s, s],
+            [s, s, s, s, s, s, s, s, s, s],
+            [s, s, s, s, s, s, s, s, s, s],
+            [d, d, d, d, d, s, s, s, s, d],
+            [d, s, s, s, s, s, s, s, s, s],
+            [s, s, s, s, s, s, s, s, s, s],
+            [s, s, s, s, s, s, s, s, s, s],
+        ]
+        posr, posc = 3, 5
+
+        expected_move_count = self.clean_board(board, posr, posc, next_move_reference)
+        result_move_count = self.clean_board(board, posr, posc, next_move)
+
+        self.assertEqual(expected_move_count, result_move_count)
 
 
 if __name__ == '__main__':
